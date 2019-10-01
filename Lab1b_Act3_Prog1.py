474a2595aec1e5d0307d9fd9f18424e1d9e87521
Andrew Lackey <drewlackey2000@gmail.com>
2019-10-01 18:03:51 -0500

    Add files via upload

---
+++ Lab1b_Act3_Prog1.py
@@ -0,0 +1,28 @@
+# By submitting this assignment, I agree to the following:
+#  “Aggies do not lie, cheat, or steal, or tolerate those who do”
+#  “I have not given or received any unauthorized aid on this assignment”
+#
+# Name: 		Andrew Lackey
+# Section:		533
+# Assignment:	Lab 1b-1 (e.g. Lab 1b-2)
+# Date:		28/8/2019 day month year
+print()
+print("My name is Andrew but most people call me Drew. I like building computers and riding longboards.")
+# A sentence about myself
+print(20*5)
+# The voltage across a conductor with resistance 20 ohms and a current of 5 amps
+print((1/2)*100*21**2)
+# The kinetic energy of an object with mass 100 kg and velocity 21 m/s
+print((1*100*2.5)/1.2)
+# The Reynolds Number for a fluid with velocity 100 m/s and kinematic viscosity 1.2 m^2/s with characteristics linear dimensions 2.5 m
+print(.15*(.45**3)/12)
+# The Moment of Inertia of a rectangular body about the x-axis with base of 15 cm and height of 45 cm
+print(4*0.387*25)
+# The Thermal Energy increase of Zinc shot put with mass of 4 kg, specific heat of 0.387, and temperature change of 25 degrees
+print((((98-73.375)**2+(56-73.375)**2+(72-73.375)**2+(85-73.375)**2+(92-73.375)**2+(45-73.375)**2+(62-73.375)**2+(77-73.375)**2)/(8-1))**(1/2))
+# The standard deviation for (98, 56, 72, 85, 92, 45, 62, 77)
+print((60*333.15)/288.15)
+# The Thermodynamics Gas Laws, determine the new presure in a container of constant volume when the starting pressure is 60 kPa
+# and the temperature moves from 15 degrees to 60 degrees Celsius (converted to kelvin)
+print(310.711*9.8)
+# Force = mass x acceleration, (weights are estimated) 190lb 160lb 165lb 170lb (converted to kg 310.711kg) acceleration 9.8m/s^2
